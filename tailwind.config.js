8f31ad730f5c66db443650e1d6c4b8a4ffa080a7
jbrorson <josefine.brorson@gmail.com>
2021-04-22 11:56:37 +0200

    Tailwind config changes, new font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -267,6 +267,7 @@ module.exports = {
         '"Segoe UI Emoji"',
         '"Segoe UI Symbol"',
         '"Noto Color Emoji"',
+        '"Lobster"',
       ],
       serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
       mono: ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
