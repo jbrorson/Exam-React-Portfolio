fc426a514acfbbe481086f865bff84e22a7582a0
jbrorson <josefine.brorson@gmail.com>
2021-04-22 21:06:41 +0200

    New Timeline component, not complete yet

---
+++ src/components/MyTimeline.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { ReactComponent as WorkIcon } from '../components/icons/work.svg';
+import { ReactComponent as EduIcon } from '../components/icons/education.svg'
+
+
+import timelineElements from './timelineElements';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+
+import "react-vertical-timeline-component/style.min.css";
+
+function MyTimeline() {
+  let workIconStyles = { background: "#06D6A0" };
+  let eduIconStyles = { background: "#3da3d5" };
+
+  return (
+    <div className="flex flex-col min-h-screen h-max-height justify-center items-center bg-green-200 p-8">
+      <h1 className="text-5xl font-bold sans:lobster text-gray-800"
+        style={{ fontFamily: 'lobster' }}>Some of my projects</h1>
+      <div className="Timeline">
+
+        <VerticalTimeline>
+          {timelineElements.map((element) => {
+            let isWorkIcon = element.icon === "work";
+
+            return (
+              <VerticalTimelineElement
+                // style={{ left: "15%", }}
+                key={element.key}
+                date={element.date}
+                dateClassName="date"
+                iconStyle={isWorkIcon ? workIconStyles : eduIconStyles}
+                icon={isWorkIcon ? <WorkIcon /> : <EduIcon />}
+              >
+                <h3 className="m-2 text-gray-700 md:text-base">
+                  {element.title}
+                </h3>
+                <h5 className="m-2 text-gray-700 text-sm md:text-base">
+                  {element.location}
+                </h5>
+                <p id="m-2 text-gray-700 text-sm md:text-base">{element.description}</p>
+              </VerticalTimelineElement>
+            );
+          })}
+        </VerticalTimeline>
+      </div>
+    </div>
+  )
+}
+
+export default MyTimeline;
