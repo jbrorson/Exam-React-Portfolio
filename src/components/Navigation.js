c731723b5878610c5dde3017a90f42231d48e395
jbrorson <josefine.brorson@gmail.com>
2021-04-22 18:17:58 +0200

    Added navlink inside Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,13 +4,13 @@ import content from '../content/index';
 function Navigation() {
   return (
     <div>
-      <div className="flex items-center justify-between w-10/12 mx-auto py-3 ">
+      <div className="z-20 flex items-center justify-between w-full fixed mx-auto p-5 bg-white">
         <h1
           style={{ fontFamily: 'Lobster' }}
-          className="text-3xl md:text-5xl font-bold">{content.nav.logo}</h1>
+          className="text-3xl ml-4 md:text-5xl font-bold text-red-500">{content.nav.logo}</h1>
         <div>
           {content.nav.links.map((link, index) => {
-            return <span key={index} className="text-xl mr-4">{link.text}</span>;
+            return <span key={index} className="text-xl text-gray-800 font-semibold px-4">{link.text}</span>;
           })}
         </div>
       </div>
