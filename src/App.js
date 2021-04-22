8e3a2093fa16a3c1eb4f97c1db1f0308408c984c
jbrorson <josefine.brorson@gmail.com>
2021-04-22 21:08:10 +0200

    Render new component to App

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Navigation from './components/Navigation';
 import Hero from './components/Hero';
 import MyStack from './components/MyStack';
 import Contact from './components/Contact';
-import MyWork from './components/MyWork';
+// import MyWork from './components/MyWork';
 import Projects from './components/Projects';
+import MyTimeline from './components/MyTimeline';
+
 import './tailwind.output.css';
 
 const App = () => (
   <div>
     <Navigation />
     <Hero />
+    <MyTimeline />
     <MyStack />
     {/* <MyWork /> */}
     <Projects />
