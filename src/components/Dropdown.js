36724e6d610df8e549e8a757bc3b33afd3b2fe30
jbrorson <josefine.brorson@gmail.com>
2021-04-28 16:11:21 +0200

    New navlinks

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import content from '../content/index';
+import { Link } from 'react-scroll';
 import Avatar from '../components/icons/avatar.svg';
 
 const Dropdown = ({ isOpen, toggle }) => {
   return (
-    <div className={isOpen ? "grid grid-rows-4 w-full fixed z-10 pt-20 text-center items-center bg-red-100"
+    <div className={isOpen ? "w-full fixed z-10 pt-20 text-center items-center bg-white"
       : "hidden"
     }
       onClick={toggle}
     >
-      {content.nav.links.map((link, index) => {
-        return <span
-          key={index}
-          className="cursor-pointer text-xl text-gray-800 font-semibold px-4 py-3 border-b-2 border-transparent hover:text-red-500">{link.text}</span>;
-      })}
-
-      <a
-        href="/https://www.facebook.com/josefine.brorson"
-        className="lg:ml-4 flex items-center lg:justify-start lg:mb-0 mb-4 cursor-pointer"
-      >
-        <img
-          src={Avatar}
-          className="bg-red-200 rounded-full w-12 h-12 mr-4 border-2 border-transparent hover:border-red-500" alt="Josefine Brorson avatar" />
-      </a>
+      <nav>
+        <ul className="flex flex-col">
+          <Link className="px-3 cursor-pointer text-xl text-gray-800 font-semibold px-4 py-3 border-b-2 border-transparent hover:bg-red-100"
+            to="home" smooth={true} duration={1500}>Home</Link>
+          <Link className="px-3 cursor-pointer text-xl text-gray-800 font-semibold px-4 py-3 border-b-2 border-transparent hover:bg-red-100"
+            to="timeline" smooth={true} duration={1500}>Experiences</Link>
+          <Link className="px-3 cursor-pointer text-xl text-gray-800 font-semibold px-4 py-3 border-b-2 border-transparent hover:bg-red-100"
+            to="about" smooth={true} duration={1500}>About</Link>
+          <Link className="px-3 cursor-pointer text-xl text-gray-800 font-semibold px-4 py-3 border-b-2 border-transparent hover:bg-red-100"
+            to="projects" smooth={true} duration={1500}>Projects</Link>
+          <Link className="px-3 cursor-pointer text-xl text-gray-800 font-semibold px-4 py-3 border-b-2 border-transparent hover:bg-red-100 mb-5"
+            to="contact" smooth={true} duration={1500}>Contact</Link>
+        </ul>
+      </nav>
+      {/* <a
+          href="/https://www.facebook.com/josefine.brorson"
+          className="lg:ml-4 flex items-center lg:justify-start lg:mb-0 mb-4 cursor-pointer"
+        >
+          <img
+            src={Avatar}
+            className="bg-red-200 rounded-full w-12 h-12 mr-4 border-2 border-transparent hover:border-red-500" alt="Josefine Brorson avatar" />
+        </a> */}
 
     </div>
   )
