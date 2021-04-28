d0c724a4d2d5c05558d421c4a754ef42ce937616
jbrorson <josefine.brorson@gmail.com>
2021-04-28 12:08:40 +0200

    Outcommented code that not nessecary for now

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import content from '../content';
+// import { LazyLoadImage } from 'react-lazy-load-image-component';
+// import content from '../content';
 
 function Projects() {
   return (
