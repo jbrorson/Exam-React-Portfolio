740ea6d873de2432ab31c6e2b4c640f6f1522ca9
jbrorson <josefine.brorson@gmail.com>
2021-05-11 15:06:07 +0200

    Not using this component

--- src/components/SingleProject.js
+++ src/components/SingleProject.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import { useLocation, useHistory } from 'react-router-dom';
+// import React from 'react';
+// import { useLocation, useHistory } from 'react-router-dom';
+// import Modal from './Modal';
 
-const SingleProject = () => {
-  const location = useLocation();
+// const SingleProject = () => {
+//   const location = useLocation();
 
-  const history = useHistory();
+//   const history = useHistory();
 
-  return (
-    <div className="min-h-screen flex items-center justify-center" id="singleproject">
-      <div className="flex flex-col justify-evenly md:flex-row md:space-x-6 space-y-6 md:space-y-0 w-full p-8 sm:p-12 sm:rounded-0 text-white overflow-hidden">
-        <h1>More info</h1>
-        <button
-          className="animate-float flex items-center space-x-2 bg-red-400 uppercase p-4 mt-8 rounded-md shadow-md text-white font-semibold sm:text-base hover:bg-red-500 transition ease-out duration-500"
-          onClick={() => {
-            history.goBack();
-          }}>Go back!!!
-      </button>
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div className="min-h-screen flex items-center justify-center" id="singleproject">
+//       <div className="flex flex-col justify-evenly md:flex-row md:space-x-6 space-y-6 md:space-y-0 w-full p-8 sm:p-12 sm:rounded-0 text-white">
+//         <Modal />
+//         <button
+//           className="animate-float flex items-center space-x-2 bg-red-400 uppercase p-4 mt-8 rounded-md shadow-md text-white font-semibold sm:text-base hover:bg-red-500 transition ease-out duration-500"
+//           onClick={() => {
+//             history.goBack();
+//           }}>Go back!!!
+//       </button>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default SingleProject;
+// export default SingleProject;
