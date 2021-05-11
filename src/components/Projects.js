85a74b28f36c6ff3da38749db98bc61280bca505
jbrorson <josefine.brorson@gmail.com>
2021-05-11 15:09:05 +0200

    Added popup to button

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,16 @@
-import React, { useEffect } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
-import { Link } from 'react-scroll';
+import React from 'react';
+// import { useHistory, useLocation } from 'react-router-dom';
+import Modal from './Modal';
+// import { Link } from 'react-scroll';
 // import { LazyLoadImage } from 'react-lazy-load-image-component';
 // import content from '../content';
 
 function Projects() {
-  const history = useHistory();
+  // const history = useHistory();
 
-  const location = useLocation();
-  useEffect(() => {
-  }, [location]);
+  // const location = useLocation();
+  // useEffect(() => {
+  // }, [location]);
 
   // const handleClick = () => {
   //   history.push("/singleproject");
@@ -37,17 +38,18 @@ function Projects() {
                 })}
               </div> */}
               <img
-                className="object-cover py-2"
+                className="object-cover"
                 src="assets/bev.png"
                 alt="Work that I have done" />
             </div>
             <div className="p-4">
               <div className="h-full flex flex-col justify-between">
-                <h1 className="m-2 text-gray-800 text-4xl font-bold md:text-2xl">B-E-V stockmarked dashboard</h1>
-                <p className="m-2 text-gray-700 text-sm md:text-base">During the investment approach, there are many questions that rises whether to sell, buy, or hold stocks.</p>
+                <h1 className="m-2 text-gray-800 text-4xl font-bold md:text-2xl">The B-E-V Dashboard</h1>
+                <p className="m-2 text-gray-700 text-sm md:text-base">B-E-V is a Stockmarket dashboard app. During the investment approach, there are many questions that rises whether to sell, buy, or hold stocks.</p>
                 <div>
                   <div>
-                    <Link to="singleproject"
+                    <Modal />
+                    {/* <Link to="singleproject"
                       smooth={true}
                       duration={1500}>
                       <button
@@ -60,7 +62,7 @@ function Projects() {
                       >
                         read more
                     </button>
-                    </Link>
+                    </Link> */}
                   </div>
                 </div>
               </div>
@@ -71,22 +73,17 @@ function Projects() {
           <div className="flex flex-col md:flex-row md:max-w-4xl max-w-sm mx-auto bg-white my-5 rounded-default sm:rounded-0 shadow-lg overflow-hidden">
             <div className="p-4 md:w-full">
               <img
-                className=""
+                className="object-cover"
                 src="assets/onboard-kyh.png"
                 alt="Work that I have done" />
             </div>
             <div className="p-4">
               <div className="h-full flex flex-col justify-between">
                 <h1 className="m-2 text-gray-800 text-4xl font-bold md:text-2xl">KYH Onboarding Site</h1>
-                <p className="m-2 text-gray-700 text-sm md:text-base">During the investment approach, there are many questions that rises whether to sell, buy, or hold stocks.</p>
+                <p className="m-2 text-gray-700 text-sm md:text-base">This website was created for a very important purpose - to help new students with their onboarding jurney.</p>
                 <div>
                   <div>
-                    <button
-                      className="inline-block self-end px-5 py-3 uppercase mt-10 bg-red-400 rounded-md shadow-md text-white text-sm font-semibold sm:text-base hover:bg-red-500 transition ease-out duration-500 transform hover:scale-105"
-                    // href="https://github.com/knoxgon/stock-market-dashboard"
-                    >
-                      read more
-                    </button>
+                    <Modal />
                   </div>
                 </div>
               </div>
