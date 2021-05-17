6f54041c90938eac33a94a38d23abf9a0fa80ba9
jbrorson <josefine.brorson@gmail.com>
2021-05-17 17:32:15 +0200

    Import new component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,21 +1,8 @@
 import React from 'react';
-// import { useHistory, useLocation } from 'react-router-dom';
 import Modal from './Modal';
-// import { Link } from 'react-scroll';
-// import { LazyLoadImage } from 'react-lazy-load-image-component';
-// import content from '../content';
+import Popup from './Popup';
 
 function Projects() {
-  // const history = useHistory();
-
-  // const location = useLocation();
-  // useEffect(() => {
-  // }, [location]);
-
-  // const handleClick = () => {
-  //   history.push("/singleproject");
-
-  // }
 
   return (
     <div className="flex flex-col min-h-screen h-max-height justify-center items-center bg-green-200 p-8" id="projects">
@@ -37,20 +24,6 @@ function Projects() {
                 <div>
                   <div>
                     <Modal />
-                    {/* <Link to="singleproject"
-                      smooth={true}
-                      duration={1500}>
-                      <button
-                        onClick={() => {
-                          history.push("/singleproject");
-                        }}
-                        type="button"
-                        className="inline-block self-end px-5 py-3 uppercase mt-10 bg-red-400 rounded-md shadow-md text-white text-sm font-semibold sm:text-base hover:bg-red-500 transition ease-out duration-500 transform hover:scale-105"
-                      // href="https://github.com/knoxgon/stock-market-dashboard"
-                      >
-                        read more
-                    </button>
-                    </Link> */}
                   </div>
                 </div>
               </div>
@@ -71,7 +44,7 @@ function Projects() {
                 <p className="m-2 text-gray-700">This website was created for a very important purpose - to help new students with their onboarding jurney.</p>
                 <div>
                   <div>
-                    <Modal />
+                    <Popup />
                   </div>
                 </div>
               </div>
