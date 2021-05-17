9d823c8b70dcff47b6231755f44bb3662c52059f
jbrorson <josefine.brorson@gmail.com>
2021-05-17 16:38:57 +0200

    Media Query for image

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,31 +21,19 @@ function Projects() {
     <div className="flex flex-col min-h-screen h-max-height justify-center items-center bg-green-200 p-8" id="projects">
       <h1 className="text-5xl font-bold text-gray-800"
         style={{ fontFamily: 'lobster' }}>Some of my projects</h1>
-      <div className="mt-10 md:grid grid-cols-2 gap-8 ">
+      <div className="mt-10 md:grid grid-cols-2 gap-8">
         <div className="block md:inline-block w-full">
-          <div className="flex flex-col md:flex-row md:max-w-4xl max-w-sm sm:w-full mx-auto bg-white my-5 rounded-default sm:rounded-0 shadow-lg over-hidden">
+          <div className="flex flex-col md:flex-row md:max-w-4xl max-w-sm mx-auto bg-white my-5 rounded-default sm:rounded-0 shadow-lg overflow-hidden">
             <div className="p-4 md:w-full">
-              {/* <div className="flex flex-wrap justify-center">
-                {content.jobs.job.map((job, index) => {
-                  return (
-                    <span
-                      className={`${index % 2 === 0 ? ' animate-float' : ' animate-refloat'
-                        } w-40 h-40 bg-white shadow-2xl m-2 rounded-full flex items-center p-5 `}
-                    >
-                      <LazyLoadImage src={job.img} alt={job.alt} />
-                    </span>
-                  );
-                })}
-              </div> */}
               <img
-                className="object-cover"
+                className="object-cover w-full"
                 src="assets/bev.png"
                 alt="Work that I have done" />
             </div>
             <div className="p-4">
-              <div className="h-full flex flex-col justify-between">
+              <div className="flex flex-col justify-between">
                 <h1 className="m-2 text-gray-800 text-4xl font-bold md:text-2xl">The B-E-V Dashboard</h1>
-                <p className="m-2 text-gray-700 text-sm md:text-base">B-E-V is a Stockmarket dashboard app. During the investment approach, there are many questions that rises whether to sell, buy, or hold stocks.</p>
+                <p className="m-2 text-gray-700">B-E-V is a Stockmarket dashboard app. During the investment approach, there are many questions that rises whether to sell, buy, or hold stocks.</p>
                 <div>
                   <div>
                     <Modal />
@@ -80,7 +68,7 @@ function Projects() {
             <div className="p-4">
               <div className="h-full flex flex-col justify-between">
                 <h1 className="m-2 text-gray-800 text-4xl font-bold md:text-2xl">KYH Onboarding Site</h1>
-                <p className="m-2 text-gray-700 text-sm md:text-base">This website was created for a very important purpose - to help new students with their onboarding jurney.</p>
+                <p className="m-2 text-gray-700">This website was created for a very important purpose - to help new students with their onboarding jurney.</p>
                 <div>
                   <div>
                     <Modal />
