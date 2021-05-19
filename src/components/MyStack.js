4791bdfe56d6c615e6b73237653484ed1c202b59
jbrorson <josefine.brorson@gmail.com>
2021-05-19 21:50:36 +0200

    Added id

--- src/components/MyStack.js
+++ src/components/MyStack.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import content from '../content';
 
-function MyStack() {
+function MyStack({ index }) {
   return (
     <div
       // style={{ background: '#ebf8ff' }}
@@ -12,10 +12,12 @@ function MyStack() {
       <h1
         className="text-5xl text-gray-800 mb-8 mt-8"
         style={{ fontFamily: 'lobster' }}>Stack I use</h1>
-      <div className="m-10 flex flex-wrap justify-center">
+      <div
+        className="m-10 flex flex-wrap justify-center">
         {content.stack.tech.map((tech, index) => {
           return (
             <span
+              key={index}
               className={`${index % 2 === 0 ? ' animate-float' : ' animate-refloat'
                 } w-40 h-40 bg-white shadow-2xl m-2 rounded-full flex items-center p-5 `}
             >
